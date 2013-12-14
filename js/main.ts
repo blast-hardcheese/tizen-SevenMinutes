5ace826762bcc1c9822f7869cb3916b146ac85ea
Devon Stewart <blast@hardchee.se>
2013-12-14 15:12:46 -0800

    Transitioning from javascript to typescript

--- js/main.ts
+++ js/main.ts
@@ -4,36 +4,40 @@
 /// <reference path="tizen.event.webidl.d.ts" />
 /// <reference path="tizen.application.webidl.d.ts" />
 /// <reference path="tizen.package.webidl.d.ts" />
+
 var backEventListener = null;
 
-var State;
-(function (State) {
-    State[State["INIT"] = 0] = "INIT";
-    State[State["EXERCISE"] = 1] = "EXERCISE";
-    State[State["FINISHED"] = 2] = "FINISHED";
-})(State || (State = {}));
+enum State {
+    INIT,
+    EXERCISE,
+    FINISHED,
+}
 
 //Initialize function
 var app = {
     refs: {
-        startButton: ".container .content .start"
+        startButton: ".container .content .start",
     },
+
     data: {
         state: undefined,
+
         timer: undefined,
         secondsRemaining: 30,
-        seq: ["init", "Jumping Jacks", "Wall-Sit", "Push-Up", "Ab-Crunch", "Chair-Step", "Squats", "Tricep Dips", "Plank", "Running", "Lunge", "Push-Up to Rotation", "Side Plank"]
+
+        seq: ["init", "Jumping Jacks", "Wall-Sit", "Push-Up", "Ab-Crunch", "Chair-Step", "Squats", "Tricep Dips", "Plank", "Running", "Lunge", "Push-Up to Rotation", "Side Plank"],
     },
+
     init: function () {
         // register once
-        if (backEventListener !== null) {
+        if ( backEventListener !== null ) {
             return;
         }
-
-        var backEvent = function (e) {
-            if (e.keyName == "back") {
-                try  {
-                    if ($.mobile.urlHistory.activeIndex <= 0) {
+        
+        var backEvent = function(e) {
+            if ( e.keyName == "back" ) {
+                try {
+                    if ( $.mobile.urlHistory.activeIndex <= 0 ) {
                         // if first page, terminate app
                         app.unregister();
                     } else {
@@ -42,76 +46,81 @@ var app = {
                         $.mobile.urlHistory.clearForward();
                         window.history.back();
                     }
-                } catch (ex) {
+                } catch( ex ) {
                     app.unregister();
                 }
             }
-        };
-
+        }
+        
         // add eventListener for tizenhwkey (Back Button)
-        document.addEventListener('tizenhwkey', backEvent);
+        document.addEventListener( 'tizenhwkey', backEvent );
         backEventListener = backEvent;
 
         // Initialization
-        $(app.refs.startButton).click(function () {
+        $(app.refs.startButton).click(function() {
             console.log("Start pressed!");
             app.start();
         });
 
-        app.become(0 /* INIT */);
+        app.become(State.INIT);
     },
-    start: function () {
+
+    start: function() {
         app.data.state = 0;
-        app.become(1 /* EXERCISE */);
+        app.become(State.EXERCISE);
         app.initializeNext();
     },
-    initializeNext: function () {
+
+    initializeNext: function() {
         app.data.state += 1;
         var curkey = app.data.seq[app.data.state];
-        if (curkey != undefined) {
+        if(curkey != undefined) {
             console.log("Switching to:", curkey);
-            app.countdown(5, function () {
+            app.countdown(5, function() {
                 console.log("countdown reached");
                 app.initializeNext();
             });
         } else {
-            app.countdown(5, function () {
+            app.countdown(5, function() {
                 console.log("End reached");
             });
         }
     },
-    become: function (state) {
-        switch (state) {
-            case 0 /* INIT */:
+
+    become: function(state: State) {
+        switch(state) {
+            case State.INIT:
                 console.log("become INIT");
                 $(".container .content .inner.before").show();
                 break;
-            case 1 /* EXERCISE */:
+            case State.EXERCISE:
                 console.log("become EXERCISE");
                 break;
-            case 2 /* FINISHED */:
+            case State.FINISHED:
                 console.log("become FINISHED");
                 break;
         }
     },
-    countdown: function (duration, callback) {
-        if (app.data.timer != undefined) {
+
+    countdown: function(duration: number, callback: Function) {
+        if(app.data.timer != undefined) {
             console.log("TIMEOUT CONFLICT");
             clearTimeout(app.data.timer);
         }
-        app.data.timer = setTimeout(function () {
+        app.data.timer = setTimeout(function() {
             app.data.timer = undefined;
             callback();
         }, duration * 1000);
     },
-    unregister: function () {
-        if (backEventListener !== null) {
-            document.removeEventListener('tizenhwkey', backEventListener);
+
+    unregister: function() {
+        if ( backEventListener !== null ) {
+            document.removeEventListener( 'tizenhwkey', backEventListener );
             backEventListener = null;
             window.tizen.application.getCurrentApplication().exit();
         }
-    }
+    },
 };
 
-$(document).bind('pageinit', app.init);
-$(document).unload(app.unregister);
+$(document).bind( 'pageinit', app.init );
+$(document).unload( app.unregister );
